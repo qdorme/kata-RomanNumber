705892385a0eb826fb5fa36b5eed6635f248944a
Quentin <qdorme@gmail.com>
2021-10-02 23:45:58 +0200

    refactoring

--- src/main/java/qdo/kata/RomanNumberConvertor.java
+++ src/main/java/qdo/kata/RomanNumberConvertor.java
@@ -17,14 +17,26 @@ public class RomanNumberConvertor {
 		return IntStream.range(0, formattedRomanNumber.length())
 				.map(index -> {
 					Integer value = RomanNumberValue.valueOf(String.valueOf(formattedRomanNumber.charAt(index))).value();
-					if (index < formattedRomanNumber.length() - 1 &&
-							RomanNumberValue.valueOf(String.valueOf(formattedRomanNumber.charAt(index))).compareTo(
-								RomanNumberValue.valueOf(String.valueOf(formattedRomanNumber.charAt(index + 1)))) < 0) {
+					if (isNegativeNumber(formattedRomanNumber, index)) {
 						value *= -1;
 					}
 					return value;
 				})
 				.sum();
+	}
+
+	private static boolean isNegativeNumber(String formattedRomanNumber, int index) {
+		return isNotTheLastCharacter(formattedRomanNumber, index)
+				&& theNextCharacterIsGreater(formattedRomanNumber, index);
+	}
+
+	private static boolean theNextCharacterIsGreater(String formattedRomanNumber, int index) {
+		return RomanNumberValue.valueOf(String.valueOf(formattedRomanNumber.charAt(index))).compareTo(
+			RomanNumberValue.valueOf(String.valueOf(formattedRomanNumber.charAt(index + 1)))) < 0;
+	}
+
+	private static boolean isNotTheLastCharacter(String formattedRomanNumber, int index) {
+		return index < formattedRomanNumber.length() - 1;
 	}
 
 	private static void isNumberValid(String romanNumber){
