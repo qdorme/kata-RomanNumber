c2059204f044314080b288051477eed35eec6feb
Quentin <qdorme@gmail.com>
2021-10-02 22:51:59 +0200

    refactoring

--- src/main/java/qdo/kata/RomanNumberConvertor.java
+++ src/main/java/qdo/kata/RomanNumberConvertor.java
@@ -5,12 +5,19 @@ import java.util.regex.Pattern;
 
 public class RomanNumberConvertor {
 
+	private static final Pattern INVALID_CHARACTERS = Pattern.compile("^[IVXLCDM]+$");
+	public static final String SPACE = "\s";
+	public static final String EMPTY = "";
+
 	public static int convert(String romanNumber) {
-		String formattedRomanNumber = romanNumber.replaceAll("\s", "").toUpperCase(Locale.ENGLISH);
-		Pattern pattern = Pattern.compile("^[IVXLCDM]+$");
-		if(!pattern.matcher(formattedRomanNumber).matches())
+		String formattedRomanNumber = romanNumber.replaceAll(SPACE, EMPTY).toUpperCase(Locale.ENGLISH);
+		isNumberValid(formattedRomanNumber);
+
+		return 0;
+	}
+
+	private static void isNumberValid(String romanNumber){
+		if(!INVALID_CHARACTERS.matcher(romanNumber).matches())
 			throw new NumberFormatException("Bad character found");
-		else
-			return 0;
 	}
 }
